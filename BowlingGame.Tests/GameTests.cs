cc445c487de63e7aba20224a9385959c6ad0b0d3
Brendon Hollingsworth <brholli@microsoft.com>
2017-06-09 14:45:22 -0400

    Add project files.

---
+++ BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+
+
+namespace BowlingGame.Tests
+{
+    public class GameTests
+    {
+         Game g;
+
+        public GameTests()
+        {
+                g = new Game();
+        }
+
+        [Fact]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.Equal(0, g.Score());
+        }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+           RollMany(20,1);
+            Assert.Equal(20, g.Score());
+        }
+        [Fact]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+            Assert.Equal(16, g.Score());
+        }
+
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+           
+            RollMany(12, 10);
+            Assert.Equal(300, g.Score());
+        }
+
+        [Fact]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            g.Roll(3);
+            g.Roll(4);
+            RollMany(16, 0);
+            Assert.Equal(24, g.Score());
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.Roll(pins);
+        }
+
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+       
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+        public void Roll(int pins)
+        {
+        
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+                if (IsStrike(frameIndex)) 
+                {
+                    score += StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(frameIndex);
+                    frameIndex += 2;
+                }
+
+            return score;
+        }
+
+
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+
+        private int SpareBonus(int frameIndex)
+        {
+            return 10 + rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private bool IsSpare(int FrameIndex)
+        {
+            return rolls[FrameIndex] + rolls[FrameIndex + 1] == 10;
+        }
+    }
+}
